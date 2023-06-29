75dede1298cac05191ce518ff2a8a806c45d459d
Jimena Palmero <jimena.palmero@macena.com.ar>
2023-06-28 21:23:00 -0300

    Add first part of project

---
+++ proyecto2023/Rules/PublicacionRule.cs
@@ -0,0 +1,14 @@
+﻿using proyecto2023.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace proyecto2023.Rules
+{
+    public class PublicacionRule
+    {
+        public Publicacion GetOnepostTratamiento()
+        {
+
+            return new Publicacion();
+        }
+    }
+}
